af80cf4891e25d17befc9e4ae7fb1029d68cf8c2
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2025-01-15 16:45:32 +0100

    new files

---
+++ firstTask.js
@@ -0,0 +1,22 @@
+let x1 = 3;
+let y1 = 4;
+let answer1 = (x1+y1)*(x1+y1);
+console.log(answer1);
+
+let value = 100;
+let answer2 = value % 10 + value; 
+console.log(answer2)
+
+let x2 = 5;
+let n = 25;
+answer3 = x2 * x2**n;
+console.log(answer3);
+
+let a = 56;
+let x = a+10;
+let y = a-10;
+let z = a%10;
+let answer4 = a/(x*y*z)
+console.log(answer4)  
+
+
---
+++ jugling.js
@@ -0,0 +1,27 @@
+var var1 = 2;
+var var2 = 3;
+var var3 = 4;
+
+temp = var1
+var1 = var2;
+var2 = var3;
+var3 = var1;
+
+console.log(var1)
+console.log(var2)
+console.log(var3)
+
+var name = "face"
+console.log(name);
+
+
+const person ={
+name:"chinedu",
+sex:"male",
+isGay:"false",
+good :false
+hobies : ["dancing","eating","gossiping"]
+};
+console.log(person);
+
+let items = [1,"ada", null , true,{}]
