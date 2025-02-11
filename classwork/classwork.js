b6ba90fd271ff18373ebabe3ca344a62a82dc510
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2025-02-11 10:59:56 +0100

    today's task

---
+++ classwork/classwork.js
@@ -0,0 +1,15 @@
+const person = { 
+    name : "john Doe",
+    age : 22 ,
+    courses : [ "maths",
+             "physics",
+             "computer sience"],
+    address :{
+        city : "new york",
+        zip :"10001"}
+    }
+
+    console.log(`students name: ${person.name}`);
+    console.log(`student second course: ${person.courses[1]}`)
+    console.log(`Zip code: ${person.address["zip"]}`)
+
